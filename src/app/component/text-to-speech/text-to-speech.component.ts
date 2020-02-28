5a31af308afd868622c8f89da4f0b1eca8999f85
andriimykytyn <andriymykytyn@yahoo.com>
2020-02-28 23:26:13 +0200

    Added components and travis integration

---
+++ src/app/component/text-to-speech/text-to-speech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-to-speech',
+  templateUrl: './text-to-speech.component.html',
+  styleUrls: ['./text-to-speech.component.scss']
+})
+export class TextToSpeechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
