aea5735eebccdfe2ce05a306fca6ce95a9c59b4f
andriimykytyn <andriymykytyn@yahoo.com>
2020-04-12 10:58:55 +0300

    Fixed bugs, added express dependency, redeploy

--- server.js
+++ server.js
@@ -2,13 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files....
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist/web'));
 
-// Send all requests to index.html
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/web/index.html'));
 });
+
+// Instruct the app to use the forceSSL middleware
+app.use(forceSSL());
 
 // default Heroku PORT 3000
 app.listen(process.env.PORT || 3000);
